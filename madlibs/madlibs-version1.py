44b5c120c32ef868cf007cdc5076aeef642652dc
aunam <aunamdosaj08@gmail.com>
2026-04-11 11:14:32 +0530

    Update and Rename

---
+++ madlibs/madlibs-version1.py
@@ -0,0 +1,17 @@
+# taking input from user
+
+# input words for the story
+adjective1 = input("enter an adjective: ")
+noun1 = input("enter a noun: ")
+adjective2 = input("enter another adjective: ")
+verb1 = input("enter a verb ending with 'ing': ")
+adjective3 = input("enter one more adjective: ")
+
+# generating the story
+print("\nyour story:\n")
+
+# printing the story
+print(f"today i went to a {adjective1} zoo.")
+print(f"in an exhibit, i saw a {noun1}.")
+print(f"{noun1} was {adjective2} and {verb1}.")
+print(f"i was {adjective3}.")
